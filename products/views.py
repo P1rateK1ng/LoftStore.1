c3e7f2814faf64beffd0ec3068adf5066440a970
Kasymov Azamat <kasymov.azamat.kk@gmail.com>
2023-03-14 21:30:06 +0600

    fixed slugs and usermodel

--- products/views.py
+++ products/views.py
@@ -1,12 +1,17 @@
 from rest_framework import mixins
-from rest_framework.viewsets import GenericViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from products.serializers import ProductModelSerializer
-from products.models import ProductModel
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from rest_framework import permissions
+from products.serializers import ProductSerializer, ProductCategorySerializer
+from products.models import ProductModel, ProductCategoryModel
 
-class ProductViewSet(mixins.CreateModelMixin,
-                   mixins.ListModelMixin,
-                   GenericViewSet):
+
+class ProductCategoryViews(ModelViewSet):
+    queryset = ProductCategoryModel.objects.all()
+    serializer_class = ProductCategorySerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class ProductViewSet(ModelViewSet):
     queryset = ProductModel.objects.all()
-    serializer_class = ProductModelSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    serializer_class = ProductSerializer
+    permission_classes = (permissions.AllowAny,)
