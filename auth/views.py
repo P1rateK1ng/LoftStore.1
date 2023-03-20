6d90eb6dd86b03554393654d62a8bc5d7e0f15f3
Kasymov Azamat <kasymov.azamat.kk@gmail.com>
2023-03-20 22:27:58 +0600

    patch on registration

--- auth/views.py
+++ auth/views.py
@@ -1,8 +1,10 @@
 from .serializers import MyTokenObtainPairSerializer, RegisterSerializer
 from users.models import UserModel
-from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import generics
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import generics, status
 
 
 class MyObtainTokenPairView(TokenObtainPairView):
@@ -14,3 +16,14 @@ class RegisterView(generics.CreateAPIView):
     queryset = UserModel.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        data = {
+            'email': user.email,
+            'access_token': str(refresh.access_token),
+        }
+        return Response(data, status=status.HTTP_201_CREATED)
